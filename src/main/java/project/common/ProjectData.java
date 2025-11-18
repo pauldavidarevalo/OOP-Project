1be6e483dd73ca19366d7a9678398008c747d15b
Paul <paul.d.arevalo@gmail.com>
2025-11-18 17:54:46 -0500

    Wrote TextReader.java and ProjectData.java. Starting writing Main. File validation and reading from input files to load data into ProjectData object.

--- src/main/java/project/common/ProjectData.java
+++ src/main/java/project/common/ProjectData.java
@@ -1,15 +1,38 @@
 package project.common;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class ProjectData {
-    /**private  data structure holding ParkingViolations
-     *  private data structure holding PropertyValues
-     *  private HashMap Zip -> Population
-     *
-     *
-     *
-     *
+    /** List of ParkingViolations
+     *  List of PropertyValues
+     *  HashMap mapping (String) Zip -> (Integer) Population
      *  get() methods to be used by processors
      */
 
+    private List<ParkingViolation> parkingViolations;
+    private List<PropertyValue> propertyValues;
+    private HashMap<String, Integer> zipPopulation;
+
+    public ProjectData(List<ParkingViolation> parkingViolations,
+                       List<PropertyValue> propertyValues,
+                       HashMap<String, Integer> zipPopulation) {
+
+        this.parkingViolations = parkingViolations;
+        this.propertyValues = propertyValues;
+        this.zipPopulation = zipPopulation;
+    }
+
+    public List<ParkingViolation> getParkingViolations() {
+        return parkingViolations;
+    }
+
+    public List<PropertyValue> getPropertyValues() {
+        return propertyValues;
+    }
+
+    public HashMap<String, Integer> getZipPopulation() {
+        return zipPopulation;
+    }
 
 }
--- src/main/java/project/data/TextReader.java
+++ src/main/java/project/data/TextReader.java
@@ -1,11 +1,36 @@
 package project.data;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
 public class TextReader {
     /** reader to parse population.txt into a HashMap mapping Zip -> Population
      * return this HashMap
-     * make the method static so no instance is needed and with filename as parameter
+     * Method is static so no instance is needed to call it with file name as
+     * parameter.
      */
-    public static HashMap <Integer, Integer >(String filename){
-        /** Turn strings to ints? or keep them strings?*/
+    public static HashMap <String, Integer > readZipPopulation(String filename){
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split("\\s+"); // split on whitespace
+                if (parts.length == 2) {
+                    String zip = parts[0];          // ZIP stays a string
+                    int population = Integer.parseInt(parts[1]);
+                    map.put(zip, population);
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return map;
     }
 }
