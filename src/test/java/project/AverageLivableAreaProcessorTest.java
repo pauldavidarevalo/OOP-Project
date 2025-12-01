38273a8f0df264e0f6d562822b217d6cac922c32
Paul <paul.d.arevalo@gmail.com>
2025-11-30 21:29:29 -0500

    Wrote Unit Tests for AverageMarketValueProcessor

---
+++ src/test/java/project/AverageLivableAreaProcessorTest.java
@@ -0,0 +1,11 @@
+package project;
+
+import project.common.*;
+import org.junit.Test;
+import project.processor.FinesPerCapitaProcessor;
+
+import java.util.*;
+import static org.junit.Assert.*;
+
+public class AverageLivableAreaProcessorTest {
+}
